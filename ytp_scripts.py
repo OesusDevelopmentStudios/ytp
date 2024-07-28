a74461a0649792313be59b525514a64de8d338da
Adam Krizar <48439304+ApocalypseArisen@users.noreplay.github.com>
2024-07-28 19:07:49 +0200

    move scripts to common folder

---
+++ ytp_scripts.py
@@ -0,0 +1,53 @@
+import os
+import requests
+import validators
+
+from pytubefix import (
+    Playlist,
+    YouTube
+)
+
+
+YT_LINK_PATTERN = "https://www.youtube."
+YT_MATCHER = '"playabilityStatus":{"status":"ERROR","reason":"Video unavailable"'
+
+
+def check_link(link: str) -> bool:
+    if not link.startswith(YT_LINK_PATTERN):
+        return False
+    if not validators.url(link):
+        return False
+    try:
+        remote = requests.get(link)
+    except:
+        return False
+    if remote.status_code != 200:
+        return False
+
+    return False if YT_MATCHER in remote.text else True
+
+
+def download(link: str) -> int:
+    video = YouTube(link)
+    title:str = video.title + ".mp3"
+
+    stream = video.streams.get_audio_only()
+    file = stream.download()
+    os.rename(file, title)
+    return 0
+
+
+def get_info(link: str) -> list[str]:
+    video = YouTube(link)
+    author = video.author
+    if " - Topic" in author:
+        author = author.replace(" - Topic", "")
+    return [video.title, author]
+
+
+def get_videos(link: str) -> list[str]:
+    links: list[str] = []
+    playlist = Playlist(link)
+    for video in playlist.videos:
+        links.append("https://www.youtube.com/watch?v=" + video.video_id)
+    return links
